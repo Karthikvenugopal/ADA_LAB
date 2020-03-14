8585df318382dd64f81866e9d5540f23c1e084af
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2020-03-14 18:50:49 +0530

    Add files via upload

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,18 +2,18 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-int factorial(int n)
+int fibonacci(int n)
 {
-	if (n==1) return 1;
-	return n*factorial(n-1);
+	if (n<=1) return n;
+	return (fibonacci(n-1)+fibonacci(n-2));
 }
 int main()
 {
 	int n;
-	cout<<"Enter a number: ";
+	cout<<"Enter the member of the fibonacci series to be determined: ";
 	cin>>n;
 	int pt = clock();
-	cout<<n<<"th fibonacci term is "<<factorial(n)<<endl;
+	cout<<n<<"th fibonacci term is "<<fibonacci(n)<<endl;
 	pt = clock() - pt;
 	cout<<"Process time: "<<(float)pt/CLOCKS_PER_SEC;
 }
