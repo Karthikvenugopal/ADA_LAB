be2e79ce9ce7b9ba2bbbd08650fe253c9cd8b5bb
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2020-04-19 10:55:20 +0530

    Update lis.cpp

--- lis.cpp
+++ lis.cpp
@@ -2,16 +2,20 @@
 #include <vector> 
 using namespace std; 
 
-void printLIS(vector<int>& arr) { 
+void printLIS(vector<int>& arr) 
+{ 
     for (int x : arr) cout << x << " "; 
     cout << endl; 
 } 
 
-void LIS(int arr[], int n) { 
+void LIS(int arr[], int n) 
+{ 
     vector<vector<int> > L(n); 
     L[0].push_back(arr[0]); 
-    for (int i = 1; i < n; i++) { 
-        for (int j = 0; j < i; j++) {
+    for (int i = 1; i < n; i++) 
+    { 
+        for (int j = 0; j < i; j++) 
+        {
             if ((arr[i] > arr[j]) && (L[i].size() < L[j].size() + 1)) L[i] = L[j]; 
         } 
         L[i].push_back(arr[i]); 
@@ -24,7 +28,8 @@ void LIS(int arr[], int n) {
 } 
   
  
-int main() { 
+int main() 
+{ 
     int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 12 }; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     LIS(arr, n); 
